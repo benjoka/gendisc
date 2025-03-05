ee761b1d35f394d002b084efd0fd10c44d630617
benjoka <ben.kraeling@study.hs-duesseldorf.de>
2025-03-05 15:29:31 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,530 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"github.com/aebruno/nwalgo"
+	"github.com/gocarina/gocsv"
+	"github.com/schollz/progressbar/v3"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+type MutationSet struct { // Our example struct, you can use "-" to ignore a field
+	Id             string `csv:"igs_id"`
+	NCount         int    `csv:"totalMissing"`
+	Substitutions  string `csv:"substitutions"`
+	Insertions     string `csv:"insertions"`
+	Deletions      string `csv:"deletions"`
+	Missings       string `csv:"missing"`
+	NonACGTNs      string `csv:"nonACGTNs"`
+	AlignmentStart int    `csv:"alignmentStart"`
+	AlignmentEnd   int    `csv:"alignmentEnd"`
+}
+
+var referenceSequence string
+var ambiguousCharacters = map[string][]string{"A": {"A"},
+	"C": {"C"},
+	"G": {"G"},
+	"T": {"T"},
+	"U": {"U"},
+	"M": {"A", "C"},
+	"R": {"A", "G"},
+	"S": {"C", "G"},
+	"W": {"A", "T"},
+	"Y": {"C", "T"},
+	"K": {"G", "T"},
+	"V": {"A", "C", "G"},
+	"H": {"A", "C", "T"},
+	"D": {"A", "G", "T"},
+	"B": {"C", "G", "T"},
+	"N": {"A", "C", "G", "T"},
+	"X": {"A", "C", "G", "T"}}
+
+func main() {
+	referenceSequence = readFromTxt("reference.txt")
+	positionMutationMap, identifierMapping, nCountMapping := extractDataFromCsv("sequences_duesseldorf_april_2022.csv")
+	start := time.Now()
+	distanceMap := make(map[int]map[int]int)
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	bar := progressbar.Default(int64((len(positionMutationMap) * (len(positionMutationMap) - 1)) / 2))
+	for i := 0; i < len(identifierMapping); i++ {
+		distanceMap[i] = make(map[int]int)
+		distanceMap[i][i] = 0
+		for j := 0; j < i; j++ {
+			wg.Add(1)
+			bar.Add(1)
+			go func(i int, j int) {
+				sequence1, sequence2 := alignSequences(positionMutationMap[i], positionMutationMap[j])
+				distance := calculateDistanceBetweenSequences(sequence1, sequence2, nCountMapping[i], nCountMapping[j])
+				mutex.Lock()
+				distanceMap[i][j] = distance
+				mutex.Unlock()
+				wg.Done()
+			}(i, j)
+		}
+		wg.Wait()
+	}
+	elapsed := time.Since(start)
+	distanceMatrix := assembleDistanceMatrix(distanceMap)
+	sortedDistanceMatrix := sortDistanceMatrix(distanceMatrix)
+	persistDistanceMatrixAsCsv(sortedDistanceMatrix, identifierMapping)
+	fmt.Printf("Time elapsed: %s\n", elapsed)
+}
+
+func extractDataFromCsv(fileName string) (map[int]map[int]map[string]string, map[int]string, map[int]int) {
+	mutationSets := readCsvFile(fileName)
+	positionMutations := make(map[int]map[int]map[string]string)
+	identifierMapping := make(map[int]string)
+	nCountMapping := make(map[int]int)
+	for i := 0; i < len(mutationSets); i++ {
+		identifierMapping[i] = mutationSets[i].Id
+		nCountMapping[i] = mutationSets[i].NCount
+		positionMutations[i] = getPositionMutationsForSequence(mutationSets[i])
+	}
+	return positionMutations, identifierMapping, nCountMapping
+}
+
+func readCsvFile(filePath string) []MutationSet {
+	sequencesFile, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer sequencesFile.Close()
+
+	mutationSets := []MutationSet{}
+
+	gocsv.SetCSVReader(func(in io.Reader) gocsv.CSVReader {
+		r := csv.NewReader(in)
+		r.Comma = ';'
+		return r
+	})
+
+	if err := gocsv.UnmarshalFile(sequencesFile, &mutationSets); err != nil {
+		panic(err)
+	}
+	if _, err := sequencesFile.Seek(0, 0); err != nil { // Go to the start of the file
+		panic(err)
+	}
+
+	sort.Slice(mutationSets[:], func(i, j int) bool {
+		return mutationSets[i].Id < mutationSets[j].Id
+	})
+
+	return mutationSets
+}
+
+func readFromTxt(filePath string) string {
+	file, _ := os.Open(filePath)
+	defer file.Close()
+	r := bufio.NewReader(file)
+	// Section 2
+	var buffer bytes.Buffer
+	for {
+		line, _, err := r.ReadLine()
+		if len(line) > 0 {
+			buffer.WriteString(string(line))
+		}
+		if err != nil {
+			break
+		}
+	}
+	return buffer.String()
+}
+
+func getPositionMutationsForSequence(mutationSet MutationSet) map[int]map[string]string {
+	positionMutations := make(map[int]map[string]string)
+	convertMutationsStringToArray(mutationSet.Missings)
+	positionMutations = extractPositionSubstitions(convertMutationsStringToArray(mutationSet.Substitutions), positionMutations)
+	positionMutations = extractPositionInsertions(convertMutationsStringToArray(mutationSet.Insertions), positionMutations)
+	positionMutations = extractPositionDeletions(convertMutationsStringToArray(mutationSet.Deletions), positionMutations)
+	positionMutations = extractPositionMissings(convertMutationsStringToArray(mutationSet.Missings), positionMutations)
+	positionMutations = extractPositionNonACGTNs(convertMutationsStringToArray(mutationSet.NonACGTNs), positionMutations)
+	positionMutations = extractPositionPaddings(mutationSet.AlignmentStart, mutationSet.AlignmentEnd, positionMutations)
+	return positionMutations
+}
+
+func convertMutationsStringToArray(mutationsString string) []string {
+	if mutationsString == "" {
+		return nil
+	}
+	mutationsArray := strings.Split(mutationsString, ",")
+	return mutationsArray
+}
+
+func extractPositionPaddings(alignmentStart int, alignmentEnd int, positionMutations map[int]map[string]string) map[int]map[string]string {
+	// csv mutation string indices start at 1
+	alignmentStart--
+	for position := 0; position < alignmentStart; position++ {
+		if positionMutations[position] == nil {
+			positionMutations[position] = make(map[string]string)
+		}
+		positionMutations[position]["del"] = "-"
+	}
+	for position := alignmentEnd; position < len(referenceSequence); position++ {
+		if positionMutations[position] == nil {
+			positionMutations[position] = make(map[string]string)
+		}
+		positionMutations[position]["del"] = "-"
+	}
+	return positionMutations
+}
+
+func extractPositionSubstitions(substitutions []string, positionMutations map[int]map[string]string) map[int]map[string]string {
+	for _, substitutionString := range substitutions {
+		positionMutations = addSubstitution(substitutionString, positionMutations)
+	}
+	return positionMutations
+}
+
+func addSubstitution(substitutionString string, positionMutations map[int]map[string]string) map[int]map[string]string {
+	re := regexp.MustCompile(`^([A-Za-z])(\d+)([A-Za-z])$`)
+	matches := re.FindStringSubmatch(substitutionString)
+	position, _ := strconv.Atoi(matches[2])
+	// csv mutation string indices start at 1
+	position--
+	character := matches[3]
+	if positionMutations[position] == nil {
+		positionMutations[position] = make(map[string]string)
+	}
+	positionMutations[position]["snp"] = character
+	return positionMutations
+}
+
+func extractPositionInsertions(insertions []string, positionMutations map[int]map[string]string) map[int]map[string]string {
+	for _, insertionString := range insertions {
+		positionMutations = addInsertion(insertionString, positionMutations)
+	}
+	return positionMutations
+}
+
+func addInsertion(insertionString string, positionMutations map[int]map[string]string) map[int]map[string]string {
+	re := regexp.MustCompile(`^(\d+):(.+)$`)
+	matches := re.FindStringSubmatch(insertionString)
+	position, _ := strconv.Atoi(matches[1])
+	// csv mutation string indices start at 1
+	position--
+	character := matches[2]
+	if positionMutations[position] == nil {
+		positionMutations[position] = make(map[string]string)
+	}
+	positionMutations[position]["ins"] = character
+	return positionMutations
+}
+
+func extractPositionDeletions(deletions []string, positionMutations map[int]map[string]string) map[int]map[string]string {
+	for _, deletionString := range deletions {
+		positionMutations = addDeletion(deletionString, positionMutations)
+	}
+	return positionMutations
+}
+
+func addDeletion(deletionString string, positionMutations map[int]map[string]string) map[int]map[string]string {
+	re := regexp.MustCompile(`^(\d+)-(\d+)$`)
+	matches := re.FindStringSubmatch(deletionString)
+	if len(matches) == 0 {
+		position, _ := strconv.Atoi(deletionString)
+		position--
+		if positionMutations[position] == nil {
+			positionMutations[position] = make(map[string]string)
+		}
+		positionMutations[position]["del"] = "-"
+		return positionMutations
+	}
+	begin, _ := strconv.Atoi(matches[1])
+	// csv mutation string indices start at 1
+	begin--
+	end, _ := strconv.Atoi(matches[2])
+	for position := begin; position < end; position++ {
+		if positionMutations[position] == nil {
+			positionMutations[position] = make(map[string]string)
+		}
+		positionMutations[position]["del"] = "-"
+	}
+	return positionMutations
+}
+
+func extractPositionMissings(missings []string, positionMutations map[int]map[string]string) map[int]map[string]string {
+	for _, missingString := range missings {
+		positionMutations = addMissing(missingString, positionMutations)
+	}
+	return positionMutations
+}
+
+func addMissing(missingString string, positionMutations map[int]map[string]string) map[int]map[string]string {
+	re := regexp.MustCompile(`^(\d+)-(\d+)$`)
+	matches := re.FindStringSubmatch(missingString)
+	if len(matches) == 0 {
+		position, _ := strconv.Atoi(missingString)
+		position--
+		if positionMutations[position] == nil {
+			positionMutations[position] = make(map[string]string)
+		}
+		positionMutations[position]["snp"] = "N"
+		return positionMutations
+	}
+	begin, _ := strconv.Atoi(matches[1])
+	// csv mutation string indices start at 1
+	begin--
+	end, _ := strconv.Atoi(matches[2])
+	for position := begin; position < end; position++ {
+		if positionMutations[position] == nil {
+			positionMutations[position] = make(map[string]string)
+		}
+		positionMutations[position]["snp"] = "N"
+	}
+	return positionMutations
+}
+
+func extractPositionNonACGTNs(nonACGTnStrings []string, positionMutations map[int]map[string]string) map[int]map[string]string {
+	for _, nonACGTnString := range nonACGTnStrings {
+		positionMutations = addNonACGTNs(nonACGTnString, positionMutations)
+	}
+	return positionMutations
+}
+
+// not tested yet
+func addNonACGTNs(nonACGTnString string, positionMutations map[int]map[string]string) map[int]map[string]string {
+	re := regexp.MustCompile(`^(\d+):(.+)$`)
+	matches := re.FindStringSubmatch(nonACGTnString)
+	position, _ := strconv.Atoi(matches[1])
+	// csv mutation string indices start at 1
+	position--
+	character := matches[2]
+	if positionMutations[position] == nil {
+		positionMutations[position] = make(map[string]string)
+	}
+	positionMutations[position]["snp"] = character
+	return positionMutations
+}
+
+func alignSequences(mutationsMapSample1 map[int]map[string]string, mutationsMapSample2 map[int]map[string]string) ([]string, []string) {
+	var sequence1 []string
+	var sequence2 []string
+
+	for referenceIndex, referenceValue := range referenceSequence {
+		referenceNucleotide := string(referenceValue)
+		var additions1 []string
+		var additions2 []string
+		mutations1, mutationsExist1 := mutationsMapSample1[referenceIndex]
+		mutations2, mutationsExist2 := mutationsMapSample2[referenceIndex]
+
+		// add reference chars to additions in case not mutations exist for the current reference position
+		if !mutationsExist1 {
+			additions1 = append(additions1, referenceNucleotide)
+		}
+		if !mutationsExist2 {
+			additions2 = append(additions2, referenceNucleotide)
+		}
+
+		// handle substitutions
+		substitutionCharacter1, substitutionExists1 := mutations1["snp"]
+		substitutionCharacter2, substitutionExists2 := mutations2["snp"]
+		if substitutionExists1 {
+			additions1 = append(additions1, substitutionCharacter1)
+		}
+		if substitutionExists2 {
+			additions2 = append(additions2, substitutionCharacter2)
+		}
+
+		// handle deletions
+		deletionCharacter1, deletionExists1 := mutations1["del"]
+		deletionCharacter2, deletionExists2 := mutations2["del"]
+		if deletionExists1 && !deletionExists2 {
+			additions1 = append(additions1, deletionCharacter1)
+		}
+		if deletionExists2 && !deletionExists1 {
+			additions2 = append(additions2, deletionCharacter2)
+		}
+
+		// handle insertions
+		insertionCharacter1, insertionExists1 := mutations1["ins"]
+		insertionCharacter2, insertionExists2 := mutations2["ins"]
+
+		if insertionExists1 && insertionExists2 && insertionCharacter1 != insertionCharacter2 {
+			alignment1, alignment2, _ := nwalgo.Align(insertionCharacter1, insertionCharacter2, 1, -1, -1)
+			additions1 = append(additions1, strings.Split(alignment1, "")...)
+			additions2 = append(additions2, strings.Split(alignment2, "")...)
+			if len(mutations1) > 1 {
+				additions1 = append(additions1, strings.Split(alignment1, "")...)
+			} else {
+				additions1 = append([]string{referenceNucleotide}, additions1...)
+				additions1 = append(additions1, strings.Split(alignment1, "")...)
+			}
+			if len(mutations2) > 1 {
+				additions2 = append(additions2, strings.Split(alignment2, "")...)
+			} else {
+				additions2 = append([]string{referenceNucleotide}, additions2...)
+				additions2 = append(additions2, strings.Split(alignment2, "")...)
+			}
+		}
+		if insertionExists1 && insertionExists2 && insertionCharacter1 == insertionCharacter2 {
+			if len(mutations1) > 1 {
+				additions1 = append(additions1, strings.Split(insertionCharacter1, "")...)
+			} else {
+				additions1 = append([]string{referenceNucleotide}, additions1...)
+				additions1 = append(additions1, strings.Split(insertionCharacter1, "")...)
+			}
+			if len(mutations1) > 1 {
+				additions2 = append(additions2, strings.Split(insertionCharacter2, "")...)
+			} else {
+				additions2 = append([]string{referenceNucleotide}, additions2...)
+				additions2 = append(additions2, strings.Split(insertionCharacter2, "")...)
+			}
+		} else if insertionExists1 && !insertionExists2 {
+			insertionLength := len(insertionCharacter1)
+			if len(mutations1) > 1 {
+				additions1 = append(additions1, strings.Split(insertionCharacter1, "")...)
+			} else {
+				additions1 = append([]string{referenceNucleotide}, additions1...)
+				additions1 = append(additions1, strings.Split(insertionCharacter1, "")...)
+			}
+			additions2 = append(additions2, strings.Split(strings.Repeat("-,", insertionLength), ",")[:insertionLength]...)
+		} else if insertionExists2 && !insertionExists1 {
+			insertionLength := len(insertionCharacter2)
+			if len(mutations2) > 1 {
+				additions2 = append(additions2, strings.Split(insertionCharacter2, "")...)
+			} else {
+				additions2 = append([]string{referenceNucleotide}, additions2...)
+				additions2 = append(additions2, strings.Split(insertionCharacter2, "")...)
+			}
+			additions1 = append(additions1, strings.Split(strings.Repeat("-,", insertionLength), ",")[:insertionLength]...)
+		}
+
+		sequence1 = append(sequence1, additions1...)
+		sequence2 = append(sequence2, additions2...)
+	}
+	return sequence1, sequence2
+}
+
+func calculateDistanceBetweenSequences(sequence1 []string, sequence2 []string, nCount1 int, nCount2 int) int {
+	distance := 0
+	sequenceLength1 := len(sequence1)
+	sequenceLength2 := len(sequence2)
+	activeGap1 := false
+	activeGap2 := false
+	properCharsThreshold := 5
+	properCharsSeen1 := 0
+	properCharsSeen2 := 0
+
+	for i := 0; i < len(sequence1); i++ {
+		character1 := sequence1[i]
+		character2 := sequence2[i]
+		if character1 == "N" || character2 == "N" {
+			continue
+		}
+
+		if character1 != "-" {
+			properCharsSeen1++
+		}
+		if character2 != "-" {
+			properCharsSeen2++
+		}
+
+		properCharsAmount1 := sequenceLength1 - nCount1
+		properCharsAmount2 := sequenceLength2 - nCount2
+		if properCharsSeen1 < properCharsThreshold ||
+			properCharsSeen2 < properCharsThreshold ||
+			(properCharsAmount1-properCharsSeen1 < properCharsThreshold) ||
+			(properCharsAmount2-properCharsSeen2 < properCharsThreshold) ||
+			character1 == character2 {
+			continue
+		}
+
+		if character1 == "-" {
+			if !activeGap1 {
+				distance++
+			}
+			activeGap1 = true
+			activeGap2 = false
+		}
+		if character2 == "-" {
+			if !activeGap2 {
+				distance++
+			}
+			activeGap1 = false
+			activeGap2 = true
+		}
+
+		if character1 != "-" && character2 != "-" &&
+			!slices.Contains(ambiguousCharacters[character1], character2) &&
+			!slices.Contains(ambiguousCharacters[character2], character1) {
+			distance++
+			activeGap1 = false
+			activeGap2 = false
+		}
+	}
+
+	return distance
+}
+
+func assembleDistanceMatrix(distanceMap map[int]map[int]int) map[int]map[int]int {
+	distanceMatrix := make(map[int]map[int]int)
+	for i := 0; i < len(distanceMap); i++ {
+		if distanceMatrix[i] == nil {
+			distanceMatrix[i] = make(map[int]int)
+		}
+		for j := 0; j <= i; j++ {
+			distance := distanceMap[i][j]
+			distanceMatrix[i][j] = distance
+			if distanceMatrix[j] == nil {
+				distanceMatrix[j] = make(map[int]int)
+			}
+			distanceMatrix[j][i] = distance
+
+		}
+	}
+	return distanceMatrix
+}
+
+func sortDistanceMatrix(distanceMatrix map[int]map[int]int) map[int]map[int]int {
+	keys := make([]int, 0, len(distanceMatrix))
+	for k := range distanceMatrix {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	sortedDistanceMatrix := make(map[int]map[int]int)
+	for _, k := range keys {
+		sortedDistanceMatrix[k] = distanceMatrix[k]
+	}
+	return sortedDistanceMatrix
+}
+
+func persistDistanceMatrixAsCsv(distanceMatrix map[int]map[int]int, identifierMapping map[int]string) {
+	file, err := os.Create("distance_matrix.csv")
+	defer file.Close()
+	if err != nil {
+		log.Fatalln("failed to open file", err)
+	}
+	w := csv.NewWriter(file)
+	w.Comma = ';'
+	defer w.Flush()
+	var data [][]string
+
+	headerRow := []string{""}
+	distanceRows := [][]string{}
+	for i := 0; i < len(distanceMatrix); i++ {
+		headerRow = append(headerRow, identifierMapping[i])
+		distances := distanceMatrix[i]
+		row := []string{identifierMapping[i]}
+		for j := 0; j < len(distanceMatrix); j++ {
+			distance := distances[j]
+			row = append(row, strconv.Itoa(distance))
+		}
+		distanceRows = append(distanceRows, row)
+	}
+	data = append(data, headerRow)
+	data = append(data, distanceRows...)
+	w.WriteAll(data)
+}
